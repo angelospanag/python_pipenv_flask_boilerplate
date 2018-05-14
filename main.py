71e41a9966503bbbc4a9e028887bc258baad3500
Angelos Panagiotopoulos <angelospanag@gmail.com>
2018-05-14 16:07:41 +0100

    Jsonify response

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    response = dict()
+    response['response'] = 'Hello World!'
+    return jsonify(response), 200
